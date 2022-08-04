5ac5e5ac9161a915451933d7f439a51bb92a9ae4
Jonathan Batalla <batallajjet@outlook.com>
2022-08-03 23:57:38 -0500

    fix all instances diceRoller

--- python101/diceRoller.py
+++ python101/diceRoller.py
@@ -1,5 +1,15 @@
 import random
+sides = ""
 print("Let's roll a dice! ")
-sides = input("How many sides should it have? (2-20): ")
+while str.isdigit(sides) == False:
+    sides = input("How many sides should it have? (2-20): ")
+    try:
+        sides = int(sides)
+        if sides < 2 or sides > 20:
+            sides = str(sides)
+            sides = ""
+    except:
+        print
+    sides = str(sides)
 sides = int(sides)
 print(random.randint(1,sides))
