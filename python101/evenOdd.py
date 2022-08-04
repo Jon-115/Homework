245c009254fd74ef0a98cae5159e1ed53cbc8fb2
Jonathan Batalla <batallajjet@outlook.com>
2022-08-03 23:20:53 -0500

    fix for all instances

--- python101/evenOdd.py
+++ python101/evenOdd.py
@@ -1,4 +1,8 @@
-num = input("Enter a number: ")
+num = ""
+while str.isdigit(num) == False:
+    num = input("Enter a number: ")
+    if num[0] == "-":
+        num = num[1:]
 num = int(num)
 if num%2 == 0:
     print("even")
