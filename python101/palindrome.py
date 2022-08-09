9f3dcb3e5f70a5d59955805a41f700dac1916081
Jonathan Batalla <batallajjet@outlook.com>
2022-08-09 09:50:47 -0500

    diff solution added

--- python101/palindrome.py
+++ python101/palindrome.py
@@ -1,10 +1,26 @@
 word = input("Enter a word. ")
 half = len(word)/2
 half = int(half)
-# print(word[0:half+1])
-# print(word[len(word):half-1:-1])
+print(word[0:half+1])
+print(word[len(word):half-1:-1])
 
 if word[0:half+1] == word[len(word):half-1:-1]:
     print("It is a palindrome. ")
 else:
     print("Not a palindrome. ")
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is same after reverse. For example 545, is the palindrome numbers
+
+userNum = (input('Please type in a palindrome number: '))
+reversedInput = (userNum[::-1])
+
+# for x in userNum:
+if userNum == reversedInput:
+    print('%s is a palindrome' % userNum)
+else:
+    print('%s is not a palindrome' % userNum)
+
+# Haven't complete for non-numbers input
